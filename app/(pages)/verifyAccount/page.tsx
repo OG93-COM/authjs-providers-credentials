ee6feb93349ec34f77f2d09bfc0a15fa8bf99d70
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-09 16:04:56 +0100

    Auth

--- app/(pages)/verifyAccount/page.tsx
+++ app/(pages)/verifyAccount/page.tsx
@@ -1,4 +1,5 @@
 import { prisma } from "@/libs/prismadb";
+import { CheckCircle } from "phosphor-react";
 
 interface verifyPageProps {
     searchParams : Promise<{token : string}>
@@ -11,8 +12,13 @@ const VerifyAccountPage = async ({searchParams} : verifyPageProps) => {
     const verifyToken = await prisma.verificationToken.findFirst({where: {token: token}})
     if(!verifyToken) return <div>Token not found</div>
     if(verifyToken.expires < new Date()) return <div>Token expired</div>
-    await prisma.user.update({where: {email: verifyToken.email}, data: {emailVerified: new Date()}})
-    return <div className="flex flex-col items-center justify-center my-10">Account Verified</div>
+    
+    try {
+      await prisma.user.update({where: {email: verifyToken.email}, data: {emailVerified: new Date()}})
+      return <div className="flex flex-col items-center justify-center my-10"><CheckCircle size={20} /> <p>Account Verified</p></div>
+    } catch (error) {
+      return <div>Error verifying account</div>
+    }
 }
 
 export default VerifyAccountPage
