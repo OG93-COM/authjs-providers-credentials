754f119db5d6d72e2e8daae45e55fa8358796391
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-09 16:03:38 +0100

    Auth

--- app/(pages)/verifyAccount/page.tsx
+++ app/(pages)/verifyAccount/page.tsx
@@ -1,14 +1,18 @@
+import { prisma } from "@/libs/prismadb";
+
 interface verifyPageProps {
     searchParams : Promise<{token : string}>
 }
 
 const VerifyAccountPage = async ({searchParams} : verifyPageProps) => {
     const currentSearchParams = await searchParams;
-  return (
-    <div>
-        Account Verified {currentSearchParams.token}
-    </div>
-  )
+    const {token} = currentSearchParams;
+
+    const verifyToken = await prisma.verificationToken.findFirst({where: {token: token}})
+    if(!verifyToken) return <div>Token not found</div>
+    if(verifyToken.expires < new Date()) return <div>Token expired</div>
+    await prisma.user.update({where: {email: verifyToken.email}, data: {emailVerified: new Date()}})
+    return <div className="flex flex-col items-center justify-center my-10">Account Verified</div>
 }
 
 export default VerifyAccountPage
