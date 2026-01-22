e57691cda7aa6f8009c47f42f1c497accf6e207d
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-23 00:48:33 +0100

    verify

--- app/(pages)/verifyAccount/page.tsx
+++ app/(pages)/verifyAccount/page.tsx
@@ -1,6 +1,6 @@
-import { prisma } from "@/libs/prismadb";
+import { verifyingEmailAction } from "@/libs/verification.action";
 import Link from "next/link";
-import { redirect } from "next/navigation";
+
 
 interface verifyPageProps {
   searchParams: Promise<{ token: string }>;
@@ -10,45 +10,23 @@ const VerifyAccountPage = async ({ searchParams }: verifyPageProps) => {
   const currentSearchParams = await searchParams;
   const { token } = currentSearchParams;
 
-  const verifyToken = await prisma.verificationToken.findFirst({
-    where: { token },
-  });
-  if (!verifyToken)
-    return (
-      <div className="flex flex-col items-center justify-center my-10">
-        {" "}
-        <p>User not found</p>
-      </div>
-    );
-  if (verifyToken.expires < new Date())
-    return <div>Lien d'Activation expiree</div>;
+  const result = await verifyingEmailAction(token)
 
-  try {
-    await prisma.user.update({
-      where: { email: verifyToken.email },
-      data: { emailVerified: new Date() },
-    });
+  
     return (
-      <div className="flex flex-col items-center justify-center">
-        <div className=" mt-10 p-2 rounded-md bg-green-200">
-          {" "}
-          <p>Account Verified</p>
-        </div>
-        <Link
-          href={"/login"}
-          className="p-2 my-2 text-sm font-bold bg-green-600 hover:bg-green-800 text-white rounded-md"
-        >
-          Go to login page
+      <div className="flex flex-col items-center justify-center h-[500px]">
+        {result?.success ? (
+          <div className="bg-green-700 text-white p-2 rounded-md">{result.message}</div>
+        ) : (
+          <div className="bg-red-700 text-white p-2 rounded-md">{result.message}</div>
+        ) }
+
+        <Link href={"/login"} className="bg-slate-700 text-white mt-5 p-2 rounded-md hover:bg-slate-500">
+          Go to Login Page
         </Link>
       </div>
     );
-  } catch (error) {
-    return (
-      <div className="flex flex-col items-center justify-center my-10 p-2 rounded-r-md bg-red-200">
-        <p>Error verifying account</p>
-      </div>
-    );
-  }
+ 
 };
 
 export default VerifyAccountPage;
---
+++ libs/verification.action.ts
@@ -0,0 +1,33 @@
+"use server"
+import { prisma } from "./prismadb"
+
+export const verifyingEmailAction = async (token:string) => {
+    try {
+        const verificationToken = await prisma.verificationToken.findUnique({where : {token}});
+        if(!verificationToken) {
+            return {success:false, message: "Token not found"}
+        }
+
+        const isExpired = new Date(verificationToken.expires) < new Date
+
+        if (isExpired) {
+            return {success:false, message: "Token is expired"}
+        }
+        const user = await prisma.user.findUnique({where : {email: verificationToken.email}})
+        if(!user) {
+            return {success:false, message: "User notfound"}
+        }
+
+        await prisma.user.update({
+            where: {id: user.id},
+            data: {emailVerified: new Date()}
+        })
+
+        await prisma.verificationToken.delete({where: {token}})
+
+        return {success:true, message: "Your Email Address Was Successfuly Verified ✅"}
+
+    } catch (error) {
+        return {success:false, message: "Something went wrong on verification"}
+    }
+}
