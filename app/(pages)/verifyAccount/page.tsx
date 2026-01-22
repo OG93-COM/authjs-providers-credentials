e8fa8a0052a39f538adc437d98a16a25b4e46b56
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-01-22 23:42:00 +0100

    verify

--- app/(pages)/verifyAccount/page.tsx
+++ app/(pages)/verifyAccount/page.tsx
@@ -1,23 +1,43 @@
 import { prisma } from "@/libs/prismadb";
 
 interface verifyPageProps {
-    searchParams : Promise<{token : string}>
+  searchParams: Promise<{ token: string }>;
 }
 
-const VerifyAccountPage = async ({searchParams} : verifyPageProps) => {
-    const currentSearchParams = await searchParams;
-    const {token} = currentSearchParams;
+const VerifyAccountPage = async ({ searchParams }: verifyPageProps) => {
+  const currentSearchParams = await searchParams;
+  const { token } = currentSearchParams;
 
-    const verifyToken = await prisma.verificationToken.findFirst({where: {token}})
-    if(!verifyToken) return <div className="flex flex-col items-center justify-center my-10"> <p>User not found</p></div>
-    if(verifyToken.expires < new Date()) return <div>Lien d'Activation expiree</div>
-    
-    try {
-      await prisma.user.update({where: {email: verifyToken.email}, data: {emailVerified: new Date()}})
-      return <div className="flex flex-col items-center justify-center my-10 p-2 rounded-r-md bg-green-200"> <p>Account Verified</p></div>
-    } catch (error) {
-      return <div className="flex flex-col items-center justify-center my-10 p-2 rounded-r-md bg-red-200"><p>Error verifying account</p></div>
-    }
-}
+  const verifyToken = await prisma.verificationToken.findFirst({
+    where: { token },
+  });
+  if (!verifyToken)
+    return (
+      <div className="flex flex-col items-center justify-center my-10">
+        <p>User not found</p>
+      </div>
+    );
+  if (verifyToken.expires < new Date())
+    return <div>Lien d'Activation expiree</div>;
 
-export default VerifyAccountPage
+  try {
+    await prisma.user.update({
+      where: { email: verifyToken.email },
+      data: { emailVerified: new Date() },
+    });
+    return (
+      <div className="flex flex-col items-center justify-center my-10 p-2 rounded-r-md bg-green-200">
+        <p>Account Verified</p>
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="flex flex-col items-center justify-center my-10 p-2 rounded-r-md bg-red-200">
+        {" "}
+        <p>Error verifying account</p>
+      </div>
+    );
+  }
+};
+
+export default VerifyAccountPage;
