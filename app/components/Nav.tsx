b49993f20dc6ce434c378b2ee622b2bae80baa87
Oussama Galai <132763749+MO5ITO@users.noreply.github.com>
2026-03-17 19:40:39 +0100

    cicd

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -12,7 +12,7 @@ export default async function Nav() {
 
 <nav className="bg-slate-900 border-gray-200 shadow-md w-full">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <div className='text-4xl text-white'>OG93 Next Auth</div>
+    <div className='text-4xl text-white'>OG93 | Auth</div>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-slate-100 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 " aria-controls="navbar-default" aria-expanded="false">
         <span className="sr-only">Open main menu</span>
         <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
